210c1ffd548c07cccdff8fd1a7d476a6035d18ea
Ben <ben@benzeng.co.uk>
2019-09-11 13:51:38 +0100

    fixed rspec

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -2,7 +2,7 @@ feature 'Add Bookmark' do
   scenario "user adds new bookmark and sees it on page" do
     visit ('/')
     fill_in 'add_bookmark', with: 'www.cricket.com'
-    click_button 'Submit'
+    click_button 'Add Bookmark'
     expect(page).to have_content 'www.cricket.com'
   end
 end
